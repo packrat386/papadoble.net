e077c4e66d49110d9528a16dc40d64865f590243
packrat386 <packrat386@gmail.com>
2014-09-06 13:25:52 -0400

    tried to fix find routing

--- routes/find.js
+++ routes/find.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/find/book', function(req, res) {
+router.get('/book', function(req, res) {
   res.render('findBook', { title: 'Find by Book' });
 });
 
-router.get('/find/ingredient', function(req, res) {
+router.get('/ingredient', function(req, res) {
   res.render('findIngredient', { title: 'Find by Ingredient' });
 });
 
-router.get('/find/id', function(req, res) {
+router.get('/id', function(req, res) {
   res.render('findId', { title: 'Find by ID' });
 });
 
-router.get('/find', function(req, res) {
+router.get('/', function(req, res) {
   res.render('find', { title: 'Find a Drink' });
 });
 
