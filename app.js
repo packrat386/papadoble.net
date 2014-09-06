794abc095e3f3deff0449cea9cbdfbd4ba17c2c0
packrat386 <packrat386@gmail.com>
2014-09-06 10:38:26 -0400

    my kingdom for some documentation

--- app.js
+++ app.js
@@ -103,7 +103,8 @@ app.use(function(err, req, res, next) {
 	});
 });
 
-var server = app.listen(80, function() {
+app.set('port', (process.env.PORT || 3000))
+var server = app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
 
